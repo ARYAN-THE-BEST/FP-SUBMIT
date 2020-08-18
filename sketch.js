6420c6f2fddf0067a41b7b03af7e5a17e21a1487
ARYAN-THE-BEST <69415015+ARYAN-THE-BEST@users.noreply.github.com>
2020-08-18 17:12:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var form,game;
+
+function setup() {
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  game = new Game();
+  game.start();
+  
+}
+
+function draw() {
+background(255)
+}
